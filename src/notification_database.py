25d8da52ec51f5560329a1574a6d7c6ec4a6c123
Michal DANEK <danek.michal@gmail.com>
2026-01-28 19:15:26 +0100

    Removed debug output
    #1

--- src/notification_database.py
+++ src/notification_database.py
@@ -34,7 +34,6 @@ class dbAccess:
     def open(self):
         # Connect to MariaDB Platform
         try:
-            print(self.config)
             self.connection = mysql.connect(
                 user=self.config['database']['username'],
                 password=self.config['database']['password'],
